c21b593aa070f604bd543f4ce143067547806aea
Dmitry Dudukin <krutoi.glad@ya.ru>
2024-01-24 00:58:12 +0300

    first commit

---
+++ Reverse_Vowels_of_a_String_345/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "race car";
+        int end = s.length() - 1;
+        int start = 0;
+        StringBuilder answer = new StringBuilder(s);
+
+        while (start < end) {
+            while (start < end && isVowels(answer.charAt(start))) {
+                ++start;
+            }
+
+            while (start < end && isVowels(answer.charAt(end))) {
+                --end;
+            }
+
+            char tmp = answer.charAt(start);
+            answer.setCharAt(start, answer.charAt(end));
+            answer.setCharAt(end, tmp);
+
+            ++start;
+            --end;
+        }
+
+        System.out.println("answer.toString() = " + answer.toString());
+    }
+
+    static boolean isVowels(char tmp) {
+        switch (tmp) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return false;
+        }
+
+        return true;
+    }
+}
